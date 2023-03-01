3b94edc8ba885a2cce18759b05760626d83096d1
Svitlana Dolintse <Svitla.m@gmail.com>
2023-03-01 02:04:06 +0200

    done

--- src/index.js
+++ src/index.js
@@ -2,12 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import './App.css';
+
 import reportWebVitals from './reportWebVitals';
+import Weather from './Weather';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    
+    <App/>
+    <Weather/>
+    <footer>
+          <a className="footer" href='https://github.com/SvitlaM/weather-app-react'> 
+          github
+          </a>
+        </footer>
   </React.StrictMode>
 );
 
